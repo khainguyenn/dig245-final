2c48183dca0f7d19d6a1787c88daacf142ba63ea
Khai Nguyen <khnguyen@davidson.edu>
2024-11-25 03:19:45 -0500

    Add quiz homepage with separate HTML, CSS, and JS files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
-
 /* javascript */
+const images = document.querySelectorAll('.quiz-container img');
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    document.querySelector('.quiz-container').innerHTML = "";
+  });
+});
+
